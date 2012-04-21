89156e99259f71a92b9fa62a2dfcb57b2f279c80
Douglas Zuniga <shanglee@gmail.com>
2012-04-20 23:00:54 -0600

    tablero basico

---
+++ ActividadUno/Gato/Celda.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Gato
+{
+    public class Celda : Button
+    {
+        public Celda()
+        {
+            Background = new SolidColorBrush(Colors.White);
+            Content = "prueba";
+            Foreground = new SolidColorBrush(Colors.Red);
+        }
+    }
+}
